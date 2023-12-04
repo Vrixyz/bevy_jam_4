5effefbd012651381443905f50f591be7b4a2d17
Thierry Berger <contact@thierryberger.com>
2023-12-04 14:16:32 +0100

    fix assets copying

--- crates/game/src/main.rs
+++ crates/game/src/main.rs
@@ -6,6 +6,7 @@ use bevy::{
     ecs::schedule::{LogLevel, ScheduleBuildSettings},
     math::vec4,
     prelude::*,
+    render::mesh::shape::Quad,
 };
 use bevy_mod_picking::prelude::*;
 use inventory::*;
@@ -82,8 +83,6 @@ impl Plugin for InventoryPlugin {
         app.add_plugins(items::interaction::Plugin);
         app.add_plugins(simple_mouse::MousePlugin);
         app.add_systems(Startup, spawn_camera);
-        app.add_systems(PostStartup, (apply_deferred, setup_selection).chain());
-        app.add_systems(Update, cycle_selection);
         app.init_resource::<RandomDeterministic>();
     }
 }
@@ -102,27 +101,22 @@ fn spawn_camera(mut commands: Commands) {
     ));
 }
 
-#[derive(Component)]
-pub struct Selection {
-    pub inventories: Vec<Entity>,
-    pub selected_index: usize,
-}
-
-fn setup_selection(
+fn spawn_share_button(
     mut commands: Commands,
-    q_inventories: Query<Entity, Or<(With<inventory::Inventory<items::ItemType>>,)>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn(Selection {
-        inventories: q_inventories.iter().collect(),
-        selected_index: 0,
-    });
-}
-
-pub fn cycle_selection(mut q_selection: Query<&mut Selection>, input: Res<Input<KeyCode>>) {
-    if input.just_pressed(KeyCode::C) {
-        let mut s = q_selection.single_mut();
-        s.selected_index += 1;
-        s.selected_index %= s.inventories.len();
-        info!("Selected: {}", s.selected_index);
-    }
+    let mesh = meshes.add(shape::Quad::default().into());
+    let mat = materials.add(ColorMaterial::from(Color::MIDNIGHT_BLUE));
+    let visual = (
+        bevy::sprite::MaterialMesh2dBundle {
+            mesh: mesh.into(),
+            transform: Transform::default().with_scale(Vec3::splat(200f32)),
+            material: mat,
+            ..default()
+        },
+        HIGHLIGHT_TINT,
+        PickableBundle::default(),
+    );
+    commands.spawn(visual);
 }
