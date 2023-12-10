5488d740022fa81db7fc227089e8f2fdf8f4000c
Eray Erdin <eraygezer.94@gmail.com>
2023-12-10 13:18:50 +0300

    add screenshot_system

--- crates/screenshot/src/lib.rs
+++ crates/screenshot/src/lib.rs
@@ -1,7 +1,25 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::view::screenshot::ScreenshotManager, window::PrimaryWindow};
 
 pub struct ScreenshotPlugin;
 
 impl Plugin for ScreenshotPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, screenshot_system);
+    }
+}
+
+// REF https://bevyengine.org/examples/Window/screenshot/
+fn screenshot_system(
+    input: Res<Input<KeyCode>>,
+    main_window: Query<Entity, With<PrimaryWindow>>,
+    mut screenshot_manager: ResMut<ScreenshotManager>,
+    mut counter: Local<u32>,
+) {
+    if input.just_pressed(KeyCode::Space) {
+        let path = format!("./screenshot-{}.png", *counter);
+        *counter += 1;
+        screenshot_manager
+            .save_screenshot_to_disk(main_window.single(), path)
+            .unwrap();
+    }
 }
